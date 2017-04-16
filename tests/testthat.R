96f1dff2eeb16d55e1233ee2f310fa3b2d95e56a
mpadge <mark.padgham@email.com>
2017-04-16 19:38:55 +0200

    add tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(sqlite3test)
+
+test_check("sqlite3test")
---
+++ tests/testthat/test-store-data.R
@@ -0,0 +1,12 @@
+context ("store data in db")
+
+require (testthat)
+
+test_that ('read and append data', {
+               #expect_silent (store_bikedata (data_dir = "..",
+               #                               bikedb = "testdb",
+               #                               quiet = TRUE))
+               #invisible (file.remove ("testdb"))
+               expect_equal (1, 1)
+})
+
